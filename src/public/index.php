c8d4819faf475c396d132892f346c9d447af55ad
mikan19 <mellamo.rina.ty@gmail.com>
2023-04-18 23:18:59 +0900

    feat: 国名を出力しました

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,6 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$countries = [1=>"ブラジル", 2=>"日本", 3=>"スペイン"];
+foreach($countries as $number => $countryName){
+    echo $number. "番目：". $countryName. "が入っています。". "\n";
+}
+
